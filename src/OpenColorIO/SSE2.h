46d1c499160f874f86195efd9ea55e12da312662
Mark Reid <mindmark@gmail.com>
2024-01-14 13:51:11 -0800

    Add more optimized version of sse2_cvtph_ps. Less instructions overall and removed branching.
    Before:
    
    runs: 50, buffer size: 8294400, random f16 <= HALF_MAX
     min      avg     max
    0.014345 0.016966 0.022971 secs
    
    runs: 50, buffer size: 8294400, random f16 full +inf+nan
     min      avg     max
    0.014615 0.018076 0.020697 secs
    
    After:
    
    runs: 50, buffer size: 8294400, random f16 <= HALF_MAX
     min      avg     max
    0.012594 0.014231 0.017868 secs
    
    runs: 50, buffer size: 8294400, random f16 full +inf+nan
     min      avg     max
    0.012393 0.014064 0.017198 secs
    
    Signed-off-by: Mark Reid <mindmark@gmail.com>

--- src/OpenColorIO/SSE2.h
+++ src/OpenColorIO/SSE2.h
@@ -129,16 +129,15 @@ static inline __m128 sse2_cvtph_ps(__m128i a)
 {
     __m128 magic      = _mm_castsi128_ps(_mm_set1_epi32((254 - 15) << 23));
     __m128 was_infnan = _mm_castsi128_ps(_mm_set1_epi32((127 + 16) << 23));
-    __m128 sign;
+    __m128i sign, nan_mask, inf_mask, ou, ou_nan, ou_inf;
     __m128 o;
-
     // the values to unpack are in the lower 64 bits
     // | 0 1 | 2 3 | 4 5 | 6 7 | 8 9 | 10 11 | 12 13 | 14 15
     // | 0 1 | 0 1 | 2 3 | 2 3 | 4 5 |  4  5 | 6   7 | 6   7
     a = _mm_unpacklo_epi16(a, a);
 
     // extract sign
-    sign = _mm_castsi128_ps(_mm_slli_epi32(_mm_and_si128(a, _mm_set1_epi32(0x8000)), 16));
+    sign = _mm_slli_epi32(_mm_and_si128(a, _mm_set1_epi32(0x8000)), 16);
 
     // extract exponent/mantissa bits
     o = _mm_castsi128_ps(_mm_slli_epi32(_mm_and_si128(a, _mm_set1_epi32(0x7fff)), 13));
@@ -146,22 +145,14 @@ static inline __m128 sse2_cvtph_ps(__m128i a)
     // magic multiply
     o = _mm_mul_ps(o, magic);
 
-    // blend in inf/nan values only if present
-    __m128i mask = _mm_castps_si128(_mm_cmpge_ps(o, was_infnan));
-    if (_mm_movemask_epi8(mask)) {
-        __m128i ou =  _mm_castps_si128(o);
-        __m128i ou_nan = _mm_or_si128(ou, _mm_set1_epi32( 0x01FF << 22));
-        __m128i ou_inf = _mm_or_si128(ou, _mm_set1_epi32( 0x00FF << 23));
+    ou = _mm_castps_si128(o);
+    nan_mask = _mm_castps_si128(_mm_cmpgt_ps(o, was_infnan));
+    inf_mask = _mm_cmpeq_epi32(ou, _mm_castps_si128(was_infnan));
 
-        // blend in nans
-        ou = sse2_blendv(ou, ou_nan, mask);
+    ou_nan = _mm_and_si128(nan_mask, _mm_set1_epi32(0x01FF << 22));
+    ou_inf = _mm_and_si128(inf_mask, _mm_set1_epi32(0x00FF << 23));
 
-        // blend in infinities
-        mask = _mm_cmpeq_epi32( _mm_castps_si128(o), _mm_castps_si128(was_infnan));
-        o  = _mm_castsi128_ps(sse2_blendv(ou, ou_inf, mask));
-    }
-
-    return  _mm_or_ps(o, sign);
+    return  _mm_castsi128_ps(_mm_or_si128(ou, _mm_or_si128(sign, _mm_or_si128(ou_nan, ou_inf))));
 }
 
 #endif
